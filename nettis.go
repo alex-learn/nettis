6bd2f7bfc6df5f1d7c3e04ea6651cc2d84985178
Am Laher <am@laher.net.nz>
2013-02-21 22:36:30 +1300

    Refactoring towards standard Go formatting

--- nettis.go
+++ nettis.go
@@ -8,31 +8,35 @@ import (
 	"os"
 )
 
-const PKG_VERSION = "0.0.4"
+// VERSION is initialised by the linker during compilation if the appropriate flag is specified:
+// e.g. go build -ldflags "-X main.VERSION 0.1.2-abcd" goxc.go
+// thanks to minux for this advice
+// So, goxc does this automatically during 'go build'
+var VERSION string
 
 var (
-	flagSet          = flag.NewFlagSet("nettis", flag.PanicOnError)
-	verbose          bool
-	listen           bool
-	initiate         bool
-	delay            int
-	http             bool
-	tls              bool
-	is_help          bool
-	is_version       bool
-	certname         string
-	keyname          string
-	trusted_certname string
+	flagSet         = flag.NewFlagSet("nettis", flag.PanicOnError)
+	verbose         bool
+	listen          bool
+	initiate        bool
+	delay           int
+	http            bool
+	tls             bool
+	isHelp          bool
+	isVersion       bool
+	certName        string
+	keyName         string
+	trustedcertName string
 )
 
-func help_text() {
+func printHelp() {
 	fmt.Fprint(os.Stderr, "nettis [options] [host:]<port>\n")
-	fmt.Fprintf(os.Stderr, " Version %s. Options:\n", PKG_VERSION)
+	fmt.Fprintf(os.Stderr, " Version '%s'. Options:\n", VERSION)
 	flagSet.PrintDefaults()
 }
 
-func version_text() {
-	fmt.Fprintf(os.Stderr, " nettis version %s\n", PKG_VERSION)
+func printVersion() {
+	fmt.Fprintf(os.Stderr, " nettis version: %s\n", VERSION)
 }
 
 func main() {
@@ -44,26 +48,26 @@ func main() {
 	flagSet.IntVar(&delay, "d", 0, "delay (seconds) before echoing")
 	flagSet.BoolVar(&http, "http", false, "initiate conversation")
 	flagSet.BoolVar(&tls, "s", false, "Secure sockets (TLS/SSL)")
-	flagSet.StringVar(&certname, "s-cert", "cert.pem", "Certificate to use for TLS")
-	flagSet.StringVar(&trusted_certname, "s-trusted-cert", "", "Trusted certificate to accept TLS (nil means trust-all)")
-	flagSet.StringVar(&keyname, "s-key", "key.pem", "Key to use for TLS")
-	flagSet.BoolVar(&is_version, "version", false, "Show version")
-	flagSet.BoolVar(&is_help, "h", false, "Show this help")
+	flagSet.StringVar(&certName, "s-cert", "cert.pem", "Certificate to use for TLS")
+	flagSet.StringVar(&trustedcertName, "s-trusted-cert", "", "Trusted certificate to accept TLS (nil means trust-all)")
+	flagSet.StringVar(&keyName, "s-key", "key.pem", "Key to use for TLS")
+	flagSet.BoolVar(&isVersion, "version", false, "Show version")
+	flagSet.BoolVar(&isHelp, "h", false, "Show this help")
 	//TODO: cert config
 	e := flagSet.Parse(call[1:])
 	if e != nil {
 		os.Exit(1)
 	}
-	if is_help {
-		help_text()
+	if isHelp {
+		printHelp()
 		return
-	} else if is_version {
-		version_text()
+	} else if isVersion {
+		printVersion()
 		return
 	}
 	remainder := flagSet.Args()
 	if len(remainder) < 1 {
-		help_text()
+		printHelp()
 		os.Exit(1)
 	}
 	port := remainder[0]
@@ -80,7 +84,7 @@ func main() {
 		}
 	} else if tls {
 		if listen {
-			transport.ListenTls(port, initiate, delay, certname, keyname, verbose)
+			transport.ListenTls(port, initiate, delay, certName, keyName, verbose)
 		} else {
 			log.Printf("TLS client unimplemented")
 			os.Exit(1)
